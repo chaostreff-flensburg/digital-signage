636d89ec6ed946a57e0e580e5e5b92114dcce900
Alexander Wunschik <alex@wunschik.net>
2017-12-02 16:17:27 +0100

    added new page: title only

---
+++ pages/_layout/h1.vue
@@ -0,0 +1,42 @@
+<template>
+	<section>
+
+		<div class="wrapper">
+			<h1 contenteditable="true">Saal 1</h1>
+		</div>
+
+	</section>
+</template>
+
+<script>
+export default {
+	layout({ params }) {
+		return params.layout.toLowerCase()
+	}
+}
+</script>
+
+<style lang="scss">
+.wrapper {
+	position: absolute;
+	z-index: 100;
+
+	width: 100vw;
+	height: 100vh;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+
+[contenteditable="true"] {
+	width: 100% !important;
+	text-align: center;
+}
+
+h1 {
+	margin: 0;
+	margin-bottom: 0.5cm;
+	font-size: 6cm;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,14 @@
 
 		<h2>WeekendHack0x3</h2>
 		<ul>
-			<li><a href="weekendhack0x3/h1h2">H1, H2</a></li>
+			<li><a href="weekendhack0x3/h1">Title only</a></li>
+			<li><a href="weekendhack0x3/h1h2">Title & Subtitle</a></li>
 		</ul>
 
 		<h2>34C3</h2>
 		<ul>
-			<li><a href="34c3/h1h2">H1, H2</a></li>
+			<li><a href="34c3/h1">Title only</a></li>
+			<li><a href="34c3/h1h2">Title & Subtitle</a></li>
 		</ul>
 
 	</section>
