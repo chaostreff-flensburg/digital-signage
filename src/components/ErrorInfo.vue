80bdddd43f38d523b41893e2236453fd83e43aed
scammo <kontakt@samuelbrinkmann.de>
2024-03-25 13:00:18 +0100

    link iger 2023

--- src/components/ErrorInfo.vue
+++ src/components/ErrorInfo.vue
@@ -12,6 +12,7 @@
                 <li><a href="/?config=hoth">hoth.info Hacks on the Habour</a></li>
                 <li><a href="/?config=eh20">#eh20 easterhegg 2023</a></li>
                 <li><a href="/?config=https://raw.githubusercontent.com/chaos-jetzt/schild-vorlagen/main/chaos_jetzt_light/">chaos.jetzt hell (Externe Resourcen)</a></li>
+                <li><a href="/?config=iger2023">IGER 2023</a></li>
             </ul>
         </div>
     </div>
